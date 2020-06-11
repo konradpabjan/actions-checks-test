a4fbdfa54944ecdbe90798514505a860399a4ca1
konradpabjan <konradpabjan@github.com>
2020-06-11 15:08:54 +0200

    Create check suite

--- action/index.js
+++ action/index.js
@@ -1,8 +1,23 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+const { GITHUB_TOKEN, GITHUB_SHA } = process.env;
+
+async function createCheck() {
+    const { data } = await octokit.checks.create({
+        ...github.context.repo,
+        name: CHECK_NAME,
+        head_sha: GITHUB_SHA,
+        status: "in_progress",
+        started_at: new Date()
+    });
+
+    console.log(data)
+}
+
 async function run(){
     console.log("this is running!")
+    createCheck()
 }
 
 run().catch(error => {
